21afada2879d52c598d67a8f620964649bc70c3f
lazyprog20 <markanthony.caldoza.iics@ust.edu.ph>
2020-02-15 22:35:51 +0800

    Edited

--- CaldozaMarkPractice1/formvalid.js
+++ CaldozaMarkPractice1/formvalid.js
@@ -10,11 +10,20 @@ function titleLimit(input){
 }
 
 function dateCheck1(startDate){
-    let endVal = document.getElementById('endDate').value;
+    let endVal = document.getElementById('endDate');
+    let postDate = document.getElementById('postDate');
 
-    if(startDate.value >= endVal && endVal != ""){
-        alert("Starting Date should be behind the final date!");
-        startDate.value = "";
+    if(postDate.value == ""){
+        if(startDate.value >= endVal.value && endVal.value != ""){
+            alert("Starting Date should be behind the final date!");
+            startDate.value = "";
+        }
+    }else if(postDate.value != ""){
+        if(startDate.value <= postDate.value){
+            alert("Starting Date should be ahead of posting date!");
+            startDate.value = "";
+            postDate.value = "";       
+        }
     }
 }
 
@@ -24,5 +33,17 @@ function dateCheck2(endDate){
     if(startVal >= endDate.value){
         alert("Final Date should be ahead from the starting date!");
         endDate.value = "";
+    }
+}
+
+function dateCheck3(postDate){
+    let startVal = document.getElementById('startDate').value;
+
+    if(postDate.value >= startVal && startVal != ""){
+        alert("Posting Date should be before the starting date!");
+        postDate.value = "";
+    }else if(startVal == ""){
+        alert("Select a start date of event first!");
+        postDate.value = "";
     }
 }
